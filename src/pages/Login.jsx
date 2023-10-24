a7e01e2531b6a51858dddd32f476337a68659c28
hello-isa <isabeljoyadriatico@gmail.com>
2023-10-25 01:20:16 +0800

    install tailwind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,16 @@
 function Login() {
   return (
     <>
-      <h1>Login Page</h1>
+      <h1 className="text-center font-bold text-5xl">
+        Clam-Pasco Dental Clinic
+      </h1>
+      <h2>Login</h2>
+      <h3>ADMIN ACCESS ONLY</h3>
+
+      <input type="text" placeholder="Username" />
+      <input type="text" placeholder="Password" />
+
+      <button>Log in</button>
     </>
   );
 }
