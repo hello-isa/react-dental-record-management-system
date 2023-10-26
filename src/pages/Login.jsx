87e00d8adf8b9d4db0efffc344b91897394e973a
hello-isa <isabeljoyadriatico@gmail.com>
2023-10-26 23:05:12 +0800

    Add icons in input box

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,12 @@
+import { FaRegUserCircle } from "react-icons/fa";
+import { FiLock } from "react-icons/fi";
+
 function Login() {
   return (
     <>
-      <div className="my-10 text-center ">
+      <div className="mt-24 text-center ">
         <h1 className="font-bold text-5xl">Clam-Pasco Dental Clinic</h1>
-        <div className="bg-cyan-600 h-0.5 my-10 mx-96"></div>
+        <div className="bg-cyan-600 h-px my-10 mx-96"></div>
       </div>
 
       <div className="my-10 space-y-4 text-center">
@@ -12,20 +15,37 @@ function Login() {
       </div>
 
       <div className="flex flex-col items-center">
-        <div className="my-5 ">
-          <input
-            className="rounded-lg border-2 border-cyan-600 h-12 w-96"
-            type="text"
-            placeholder="Username"
-          />
-        </div>
+        {/* Username input box */}
         <div className="my-5">
-          <input
-            className="rounded-lg border-2 border-cyan-600 h-12 w-96"
-            type="text"
-            placeholder="Password"
-          />
+          <div className="relative">
+            <FaRegUserCircle
+              className="absolute top-1/2 left-4 transform -translate-y-1/2 text-black"
+              size={22}
+            />
+            <input
+              className="pl-12 rounded-lg border-2 border-cyan-600 h-12 w-96"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
         </div>
+
+        {/* Password input box */}
+        <div className="my-5">
+          <div className="relative">
+            <FiLock
+              className="absolute top-1/2 left-4 transform -translate-y-1/2 text-black"
+              size={22}
+            />
+            <input
+              className="pl-12 rounded-lg border-2 border-cyan-600 h-12 w-96"
+              type="text"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+
+        {/* Log in button */}
         <div className="my-5">
           <button className="rounded-lg border-2 h-12 w-96 bg-green-400 text-white">
             Log in
