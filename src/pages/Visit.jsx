c1fad73f3ec6e6b0d77482560da03906b461f122
hello-isa <isabeljoyadriatico@gmail.com>
2023-10-27 22:27:31 +0800

    Fetch REST API data

--- src/pages/Visit.jsx
+++ src/pages/Visit.jsx
@@ -1,7 +1,37 @@
+import React, { useState, useEffect } from "react";
+
 function Visit() {
+  const [visits, setVisits] = useState([]);
+
+  const fetchVisitData = () => {
+    const jsonFileUrl =
+      "https://raw.githubusercontent.com/hello-isa/react-dental-record-management-system/main/visit-page-mock-data.json";
+
+    fetch(jsonFileUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setVisits(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchVisitData();
+  }, []);
+
   return (
     <>
       <h1>Visit Page</h1>
+      {visits.map((visit) => (
+        <div key={visit.id}>
+          <div>{visit.date}</div>
+          <div>{visit.visit_purpose}</div>
+          <div>{visit.treatment}</div>
+          <div>{visit.balance}</div>
+          <br />
+        </div>
+      ))}
     </>
   );
 }
