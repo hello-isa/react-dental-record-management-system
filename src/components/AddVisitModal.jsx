00b239ff2ea88b6649756ee299a949e7dceaffa5
hello-isa <isabeljoyadriatico@gmail.com>
2023-11-20 15:20:31 +0800

    Create visit modal

---
+++ src/components/AddVisitModal.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+
+function AddVisitModal({ isVisible, onClose }) {
+  if (!isVisible) return null;
+
+  return (
+    <>
+      <div className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center">
+        <div className="w-3/4 flex flex-col">
+          <button
+            className="text-white text-xl place-self-end"
+            onClick={() => onClose()}
+          >
+            <IoMdCloseCircleOutline />
+          </button>
+          <div className="bg-white p-2 rounded-md">Modal</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AddVisitModal;
