e063fa03d64d1ca7c99400f6f535a9e243c87e82
hello-isa <isabeljoyadriatico@gmail.com>
2023-11-21 21:48:21 +0800

    Add stylings

--- src/components/ViewModal.jsx
+++ src/components/ViewModal.jsx
@@ -18,19 +18,60 @@ function ViewModal({ isVisible, onClose, rowData }) {
           {/* Modal content */}
           <div className="bg-white p-4 rounded-md">
             <div className="grid grid-cols-2 gap-4">
-              <p>
-                <strong>Date:</strong> {rowData.date}
-              </p>
-              <p>
-                <strong>Visit Purpose:</strong> {rowData.visit_purpose}
-              </p>
-              <p>
-                <strong>Treatment:</strong> {rowData.treatment}
-              </p>
-              <p>
-                <strong>Balance:</strong> {rowData.balance}
-              </p>
+              {/* Display data */}
+              <div className=" border border-gray-300 p-2 rounded-lg">
+                <p>
+                  <strong>Date:</strong> {rowData.date}
+                </p>
+              </div>
+              <div className=" border border-gray-300 p-2 rounded-lg">
+                <p>
+                  <strong>Visit Purpose:</strong> {rowData.visit_purpose}
+                </p>
+              </div>
+              <div className=" border border-gray-300 p-2 rounded-lg">
+                <p>
+                  <strong>Treatment:</strong> {rowData.treatment}
+                </p>
+              </div>
+              <div className=" border border-gray-300 p-2 rounded-lg">
+                <p>
+                  <strong>Prescription:</strong> {rowData.prescription}
+                </p>
+              </div>
+              <div className=" border border-gray-300 p-2 rounded-lg">
+                <p>
+                  <strong>Notes:</strong> {rowData.notes}
+                </p>
+              </div>
             </div>
+
+            <hr className="my-2 border-gray-300 my-6" />
+
+            <div className="grid grid-cols-2 gap-4">
+              <div className=" border border-gray-300 p-2 rounded-lg">
+                <p>
+                  <strong>Total Fee:</strong> {rowData.total_fee}
+                </p>
+              </div>
+              <div className=" border border-gray-300 p-2 rounded-lg">
+                <p>
+                  <strong>Discount:</strong> {rowData.discount}
+                </p>
+              </div>
+              <div className=" border border-gray-300 p-2 rounded-lg">
+                <p>
+                  <strong>Amount Paid:</strong> {rowData.amount_paid}
+                </p>
+              </div>
+              <div className=" border border-gray-300 p-2 rounded-lg">
+                <p>
+                  <strong>Balance:</strong> {rowData.balance}
+                </p>
+              </div>
+            </div>
+
+            <hr className="my-2 border-gray-300 my-6" />
           </div>
         </div>
       </div>
